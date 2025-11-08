e1cb1ace880f3cc6cd52b1542fc0489f075a804a
Grace Ghansah <gracegh@stlghana.com>
2025-11-08 23:55:19 +0000

    update

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,11 +1,8 @@
-#!/usr/bin/node
-
 const arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let result = "";
 
 for (let i = 0; i < arr.length; i++) {
-  result += arr[i];
-  if (i < arr.length - 1) result += "\n";
+  result += arr[i] + "\n";
 }
 
-console.log(result);
+console.log(result.trim());
