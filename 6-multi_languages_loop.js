4babbea8b13c181cfa3072dff50470ea2a2ecdcc
Grace Ghansah <gracegh@stlghana.com>
2025-11-08 23:53:44 +0000

    working with loops updated again

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,12 +1,11 @@
+#!/usr/bin/node
+
 const arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let result = "";
 
-
-// for(let i=0; i<arr.length; i++){
-//     console.log(arr[i]);
-// }
-let i;
-
-while(i < arr.length){
-  console.log(arr[i]);
-    i++;
+for (let i = 0; i < arr.length; i++) {
+  result += arr[i];
+  if (i < arr.length - 1) result += "\n";
 }
+
+console.log(result);
